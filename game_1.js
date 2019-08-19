4c13133490cecca2276df208d3fbcef0552c7628
Biswaranjan Sahoo <51198150+biswaranjan05@users.noreply.github.com>
2019-08-19 16:20:20 +0530

    Update game_1.js

--- game_1.js
+++ game_1.js
@@ -61,7 +61,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
          //check if player own the game
          if (scores[activePlayer] >= 10){
-           document.querySelector('#name-' + activePlayer).textContent = 'Winner Winner Dirty Fucker!' ;
+           document.querySelector('#name-' + activePlayer).textContent = 'Congrats Winner!' ;
            document.querySelector('.dice').style.display = 'none';
            document.querySelector('.player-' +activePlayer + '-panel').classList.add('winner');
            document.querySelector('.player-' +activePlayer + '-panel').classList.remove('active');
